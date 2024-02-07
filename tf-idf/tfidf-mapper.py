028c09303d20a35b790ec3eec832933309813292
Glibba <137692808+Glibba@users.noreply.github.com>
2024-02-07 16:02:47 +0900

    Update tfidf-mapper.py

--- tf-idf/tfidf-mapper.py
+++ tf-idf/tfidf-mapper.py
@@ -2,8 +2,13 @@
 
 import sys
 
+words = {}
+
 for line in sys.stdin:
     input = line.strip().split(' ')
     doc = input[0]
+
+    prev_word = words.get()
     for word in input[1:]:
+        if 
         print(word, value, 1)
